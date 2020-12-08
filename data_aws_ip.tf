e230ba24e602dd7a045d02e8618fd7b4bd1f628d
Kav <kav@33n.co.uk>
2020-12-08 17:55:47 +0000

    try ec2 ip ranges

--- data_aws_ip.tf
+++ data_aws_ip.tf
@@ -2,3 +2,8 @@ data "aws_ip_ranges" "eu_west_2_codebuild" {
   regions  = ["eu-west-2"]
   services = ["codebuild"]
 }
+
+data "aws_ip_ranges" "eu_west_2_ec2" {
+  regions  = ["eu-west-2"]
+  services = ["ec2"]
+}
