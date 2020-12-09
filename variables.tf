1dc2d14e466fd40baadcaf953adc6024bdae4059
Kav <kav@33n.co.uk>
2020-12-09 16:00:30 +0000

    cleanup

--- variables.tf
+++ variables.tf
@@ -26,12 +26,6 @@ variable "encrypt_ami" {
   default = true
 }
 
-variable "kms_key_arn" {
-  description = "If Encrypt_ami set to true then you must pass in the arn of the key you wish to encrypt disk with."
-  default     = "Default"
-  type        = string
-}
-
 variable "environment_build_image" {
   type        = string
   default     = "aws/codebuild/standard:1.0"
